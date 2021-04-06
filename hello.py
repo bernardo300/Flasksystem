3c7fa9918a1bee7b5f4fd7f6a59b4b741edca340
bernardo300 <bernardolopes.luz@hotmail.com>
2021-04-06 17:31:30 -0300

    timestamp

--- hello.py
+++ hello.py
@@ -2,13 +2,20 @@ from flask import abort
 from flask import Flask
 from flask import redirect, render_template
 
+from flask_bootstrap import Bootstrap
+
+from flask_moment import Moment
+from datetime import datetime
+
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -22,6 +29,16 @@ def get_user(id):
     if not user:
         abort(404)
     return '<h1>Hello, {}</h1>'.format(user.name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 #app.add_url_rule('/', 'index', index)
