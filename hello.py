0e49d13eb17358052bb349e23d268c3d3cb7e35a
bernardo300 <bernardolopes.luz@hotmail.com>
2021-04-08 09:32:38 -0300

    message de alerta

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
 from wtforms.validators import DataRequired
 from wtforms import StringField, SubmitField, IntegerField
 from flask_wtf import FlaskForm
@@ -17,23 +18,25 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-# @app.route('/')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = 'Gabriel'
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
-@app.route('/user/<name>')
+@ app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
 
-@app.route('/user/<id>')
+@ app.route('/user/<id>')
 def get_user(id):
     user = load_user(id)
     if not user:
@@ -41,12 +44,12 @@ def get_user(id):
     return '<h1>Hello, {}</h1>'.format(user.name)
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.errorhandler(500)
+@ app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
@@ -55,4 +58,4 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     age = IntegerField('How is old?', validators=[DataRequired()])
     submit = SubmitField('Submit')
-    #app.add_url_rule('/', 'index', index)
+    # app.add_url_rule('/', 'index', index)
